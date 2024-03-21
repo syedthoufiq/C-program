089f78be9ca02cb36a0f61fa008d02bbaa08e999
syedthoufiq <124244867+syedthoufiq@users.noreply.github.com>
2024-03-21 14:48:21 +0530

    Add files via upload

---
+++ 3rd star pt.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int rows = 10;
+	for(int i=0;i<rows;i++) {
+		for(int j=0;j<=i;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ 4th cmp str.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str1[20];
+	char str2[20];
+	int value;
+	printf("Enter first string:");
+	scanf("%s",str1);
+	printf("Enter second string:");
+	scanf("%s",str2);
+	value = strcmp(str1, str2);
+	if(value==0) 
+	{
+	printf("strings are same.");
+	} else {
+	printf("strings are not same.");
+	}
+	return 0;
+}
